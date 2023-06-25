14568dc152e789051383986871649128d1f700d5
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-06-25 17:54:32 +0530

    Create Q-01.js

---
+++ Q-01.js
@@ -0,0 +1,14 @@
+// Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Ans-1      
+
+
+* @mohit {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ 
+var moveZeroes = function(nums) {
+    
+};
